4b78b5b44a9845863a35743c3c815ae23ddc2529
CodedLibra <adeola.ogundijo@yahoo.com>
2024-08-21 13:40:51 +0100

    feat: created api route for POST request to add an atachment to a course

---
+++ app/api/courses/[courseId]/attachments/route.ts
@@ -0,0 +1,45 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { courseId } = params;
+    const url = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const courseOwner = await db.course.findUnique({
+      where: {
+        id: courseId,
+        userId: userId,
+      },
+    });
+
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const attachment = await db.attachment.create({
+      data: {
+        url: url.url,
+        name: url.url.split("/").pop(),
+        courseId: params.courseId,
+      },
+    });
+    console.log(url.url.split("/").pop());
+
+    return NextResponse.json(attachment);
+  } catch (error) {
+    console.log("[COURSEID_ATTACHMENTS]", error);
+    return new NextResponse("Internal Server Error", {
+      status: 500,
+    });
+  }
+}
