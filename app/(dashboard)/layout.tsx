9f0b527970a8b9ae4ffb6c0991f87624bf41b12b
CodedLibra <adeola.ogundijo@yahoo.com>
2024-08-21 13:08:51 +0100

    chore

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -18,18 +18,14 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
-      <body>
-        <div className="h-full">
-          <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
-            <Navbar />
-          </div>
-          <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
-            <Sidebar />
-          </div>
-          <main className="md:pl-56 pt-[80px] h-full">{children}</main>
-        </div>
-      </body>
-    </html>
+    <div className="h-full">
+      <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+        <Navbar />
+      </div>
+      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+        <Sidebar />
+      </div>
+      <main className="md:pl-56 pt-[80px] h-full">{children}</main>
+    </div>
   );
 }
