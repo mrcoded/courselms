0dabfd869acd9296f5a94dd3d6fa5e3b8e74a3a7
CodedLibra <adeola.ogundijo@yahoo.com>
2024-08-21 13:34:51 +0100

    feat: built an image-form component with image uploads and PATCH api to allow for course image uploads

---
+++ app/(dashboard)/(routes)/tutor/courses/[courseId]/_components/image-form.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { z } from "zod";
+import axios from "axios";
+import { FileUpload } from "@/components/file-upload";
+import { Course } from "@prisma/client";
+
+import { ImageIcon, Pencil, PlusCircle } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { toast } from "@/components/ui/use-toast";
+
+interface ImageFormProps {
+  initialData: Course;
+  courseId: string;
+}
+
+const ImageForm = ({ initialData, courseId }: ImageFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = () => setIsEditing((prev) => !prev);
+
+  const router = useRouter();
+
+  const formSchema = z.object({
+    imageUrl: z.string().min(1, {
+      message: "imageUrl is required",
+    }),
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values);
+      toast({ title: "Success", description: "Course successfully updated" });
+      toggleEdit();
+      router.refresh();
+    } catch (error) {
+      toast({ title: "Error", description: "Something went wrong" });
+    }
+  };
+
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course Descrption
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing && <>Cancel</>}
+          {!isEditing && !initialData.imageUrl && (
+            <>
+              <PlusCircle className="h-4 w-4 mr-2" />
+              Add an image
+            </>
+          )}
+          {!isEditing && initialData.imageUrl && (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit image
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing &&
+        (!initialData.imageUrl ? (
+          <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
+            <ImageIcon className="h-10 w-10 text-slate-500" />
+          </div>
+        ) : (
+          <div className="relative aspect-video mt-2">
+            <Image
+              src={initialData.imageUrl}
+              alt="Upload"
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+        ))}
+
+      {isEditing && (
+        <div>
+          <FileUpload
+            endpoint="courseImage"
+            onchange={(url) => {
+              if (url) {
+                onSubmit({ imageUrl: url });
+              }
+            }}
+          />
+          <div className="text-xs text-muted-foreground mt-4">
+            16:9 aspect ratio recommended
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ImageForm;
