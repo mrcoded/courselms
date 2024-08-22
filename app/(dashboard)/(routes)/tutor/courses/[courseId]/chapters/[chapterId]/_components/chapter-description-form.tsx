085afa7d028ec00cb9bfb4cbc64aa233753e39fb
CodedLibra <adeola.ogundijo@yahoo.com>
2024-08-22 23:44:09 +0100

    feat: built a chapter-description-form component with form validation to allow an updated course descriptions

---
+++ app/(dashboard)/(routes)/tutor/courses/[courseId]/chapters/[chapterId]/_components/chapter-description-form.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { z } from "zod";
+import { Chapter } from "@prisma/client";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Pencil } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+import {
+  Form,
+  FormControl,
+  FormMessage,
+  FormField,
+  FormItem,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { toast } from "@/components/ui/use-toast";
+import { Editor } from "@/components/editor";
+import { Preview } from "@/components/preview";
+
+interface ChapterDescriptionFormProps {
+  initialData: Chapter;
+  courseId: string;
+  chapterId: string;
+}
+
+const formSchema = z.object({
+  description: z.string().min(1),
+});
+
+const ChapterDescriptionForm = ({
+  initialData,
+  courseId,
+  chapterId,
+}: ChapterDescriptionFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = () => setIsEditing((prev) => !prev);
+
+  const router = useRouter();
+
+  const formMethods = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      description: initialData?.description || "",
+    },
+  });
+
+  const { isSubmitting, isValid } = formMethods.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(
+        `/api/courses/${courseId}/chapters/${chapterId}`,
+        values
+      );
+      toast({ title: "Success", description: "Chapter Description updated" });
+      toggleEdit();
+      router.refresh();
+    } catch (error) {
+      toast({ title: "Error", description: "Something went wrong" });
+    }
+  };
+
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Chapter Descrption
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit descrption
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <div
+          className={cn(
+            "text-sm mt-2",
+            !initialData.description && "text-slate-500 italic"
+          )}
+        >
+          {!initialData.description && "No description"}
+          {initialData.description && (
+            <Preview value={initialData.description} />
+          )}
+        </div>
+      )}
+
+      {isEditing && (
+        <Form {...formMethods}>
+          <form
+            onSubmit={formMethods.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
+            <FormField
+              control={formMethods.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Editor {...field} />
+                  </FormControl>
+                  <FormMessage />
+                  <div className="flex items-center gap-x-2">
+                    <Button type="submit" disabled={!isValid || isSubmitting}>
+                      Save
+                    </Button>
+                  </div>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
+
+export default ChapterDescriptionForm;
