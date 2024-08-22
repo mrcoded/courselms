286fcbc17b28cbfd1e75de133c70d2bc63f0cd86
CodedLibra <adeola.ogundijo@yahoo.com>
2024-08-22 23:59:02 +0100

    feat: created api route for PATCH request to remove the publish update  request of a chapter of  a  course

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/unpublish/route.ts
@@ -0,0 +1,63 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+
+import { db } from "@/lib/db";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const courseOwner = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId: userId,
+      },
+    });
+
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const unPublishedChapter = await db.chapter.update({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+      data: {
+        isPublished: false,
+      },
+    });
+
+    const publishedChaptersInCourse = await db.chapter.findMany({
+      where: {
+        courseId: params.courseId,
+        isPublished: true,
+      },
+    });
+
+    if (!publishedChaptersInCourse.length) {
+      await db.course.update({
+        where: {
+          id: params.courseId,
+        },
+        data: {
+          isPublished: false,
+        },
+      });
+    }
+
+    return NextResponse.json(unPublishedChapter);
+  } catch (error) {
+    console.log("[CHAPTERID_UNPUBLISH]", error);
+    return new NextResponse("Internal Server Error", {
+      status: 500,
+    });
+  }
+}
