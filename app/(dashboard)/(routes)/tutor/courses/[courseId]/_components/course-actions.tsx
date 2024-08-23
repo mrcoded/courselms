f7614eee5ae74079441760e166907f9de98a33e8
CodedLibra <adeola.ogundijo@yahoo.com>
2024-08-23 23:48:41 +0100

    feat: created a course-actions component to publish and delete course

---
+++ app/(dashboard)/(routes)/tutor/courses/[courseId]/_components/course-actions.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { useState } from "react";
+import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { ConfirmModal } from "@/components/modals/confirm-modal";
+import axios from "axios";
+import { Button } from "@/components/ui/button";
+import { toast } from "@/components/ui/use-toast";
+
+interface CourseActionsProps {
+  disabled: boolean;
+  isPublished: boolean;
+  courseId: string;
+}
+
+const CourseActions = ({
+  courseId,
+  isPublished,
+  disabled,
+}: CourseActionsProps) => {
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      if (isPublished) {
+        await axios.patch(`/api/courses/${courseId}/unpublish`);
+        toast({
+          title: "Success",
+          description: "Course Unpublished",
+        });
+      } else {
+        await axios.patch(`/api/courses/${courseId}/publish`);
+        toast({
+          title: "Success",
+          description: "Course published",
+        });
+      }
+
+      router.refresh();
+    } catch {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: "Something went wrong",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+
+      await axios.delete(`/api/courses/${courseId}`);
+      toast({
+        title: "Success",
+        description: "Course deleted",
+      });
+      router.refresh();
+      router.push(`/tutor/courses`);
+    } catch {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: "Something went wrong",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex items-center gap-x-2">
+      <Button
+        onClick={onClick}
+        disabled={disabled || isLoading}
+        variant="outline"
+        size="sm"
+      >
+        {isPublished ? "Unpublish" : "Publish"}
+      </Button>
+      <ConfirmModal onConfirm={onDelete}>
+        <Button size="sm" disabled={isLoading}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </ConfirmModal>
+    </div>
+  );
+};
+
+export default CourseActions;
