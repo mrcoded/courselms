f8ce8cc786add47fa81e2f2c75f72bc53220e23b
CodedLibra <adeola.ogundijo@yahoo.com>
2024-08-28 23:30:27 +0100

    feat: built a use-debounce hook to handle not sending loads of queries to the db when users try to search for ac course

---
+++ hooks/use-debounce.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export function useDebounce<T>(value: T, delay?: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  //only performs search when user stops typing,
+  //instead of multiple requeests to db for queries
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay || 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
