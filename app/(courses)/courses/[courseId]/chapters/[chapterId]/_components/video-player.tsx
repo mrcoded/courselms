025eb8541dd5fbe89db02ca12e4413fdb0455bcf
CodedLibra <adeola.ogundijo@yahoo.com>
2024-08-29 18:37:46 +0100

    feat: created a videoplayer component to play attached video to chapter on page load

---
+++ app/(courses)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import React, { useState } from "react";
+import { Loader2, Lock } from "lucide-react";
+
+import MuxPlayer from "@mux/mux-player-react";
+import { cn } from "@/../../lib/utils";
+
+interface VideoPlayerProps {
+  playbackId: string;
+  courseId: string;
+  chapterId: string;
+  nextChapterId?: string;
+  isLocked: boolean;
+  completeOnEnd: boolean;
+  title: string;
+}
+
+const VideoPlayer = ({
+  playbackId,
+  courseId,
+  chapterId,
+  nextChapterId,
+  isLocked,
+  completeOnEnd,
+  title,
+}: VideoPlayerProps) => {
+  const [isReady, setIsReady] = useState(false);
+
+  return (
+    <div className="relative aspect-video">
+      {!isReady && !isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
+          <Loader2 className="w-8 h-8 animate-spin text-secondary" />
+        </div>
+      )}
+      {isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
+          <Lock className="w-8 h-8" />
+          <p className="text-sm">This chapter is locked</p>
+        </div>
+      )}
+      {!isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
+          <MuxPlayer
+            title={title}
+            className={cn(
+              "h-auto w-auto md:h-2/3 md:w-1/2",
+              !isReady && "hidden"
+            )}
+            playbackId={playbackId}
+            onCanPlay={() => setIsReady(true)}
+            onEnded={() => {}}
+            autoPlay
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default VideoPlayer;
