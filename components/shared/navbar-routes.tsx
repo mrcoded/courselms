9c66e435a3a71441fb8a1ae3877c39d42e89d3d4
CodedLibra <adeola.ogundijo@yahoo.com>
2024-08-29 18:47:44 +0100

    feat: added searchinput component to navbar

--- components/shared/navbar-routes.tsx
+++ components/shared/navbar-routes.tsx
@@ -8,37 +8,41 @@ import Link from "next/link";
 import { Button } from "../ui/button";
 import { LogOut } from "lucide-react";
 
+import SearchInput from "../search-input";
+
 const NavbarRoutes = () => {
   const pathname = usePathname();
 
   const { isAuthenticated } = useKindeBrowserClient();
 
   const isTutorPage = pathname?.startsWith("/tutor");
-  const isCoursePage = pathname?.startsWith("/chapter");
+  const isCoursePage = pathname?.startsWith("/courses");
+  const isSearchPage = pathname === "/search";
 
   return (
-    <div className="flex gap-x-2 ml-auto items-center">
-      {isTutorPage || isCoursePage ? (
-        <Link href="/">
-          <Button size="sm" variant="ghost">
-            <LogOut className="h-4 w-4 mr-2" /> Exit
-          </Button>
-        </Link>
-      ) : (
-        <Link href="/tutor/courses">
-          <Button size="sm" variant="ghost">
-            Tutor Dashboard
-          </Button>
-        </Link>
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
       )}
-      {isAuthenticated ? (
-        <LoginLink>Login</LoginLink>
-      ) : (
-        <>
-          <LogOut />
-        </>
-      )}
-    </div>
+      <div className="flex gap-x-2 ml-auto items-center">
+        {isTutorPage || isCoursePage ? (
+          <Link href="/">
+            <Button size="sm" variant="ghost">
+              <LogOut className="h-4 w-4 mr-2" /> Exit
+            </Button>
+          </Link>
+        ) : (
+          <Link href="/tutor/courses">
+            <Button size="sm" variant="ghost">
+              Tutor Dashboard
+            </Button>
+          </Link>
+        )}
+        {!isAuthenticated && <LoginLink>Login</LoginLink>}
+      </div>
+    </>
   );
 };
 
