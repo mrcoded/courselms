24887b289cdc3bb2b18729723c827d25e9a53017
CodedLibra <adeola.ogundijo@yahoo.com>
2024-08-29 19:14:27 +0100

    chore

--- app/(dashboard)/(routes)/tutor/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
+++ app/(dashboard)/(routes)/tutor/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
@@ -93,7 +93,7 @@ const ChapterVideoForm = ({
             }}
           />
           <div className="text-xs text-muted-foreground mt-4">
-            Upload this chapter's video
+            Upload this chapter`&apos;`s video
           </div>
         </div>
       )}
