00fccb451007ff4ff67284bb2dee01603edc5be8
CodedLibra <adeola.ogundijo@yahoo.com>
2024-09-02 20:21:34 +0100

    feat: added CourseProgress to coursesidebar component on successful purchase

--- app/(courses)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(courses)/courses/[courseId]/_components/course-sidebar.tsx
@@ -3,9 +3,10 @@ import { redirect } from "next/navigation";
 
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { Chapter, Course, UserProgress } from "@prisma/client";
+import { db } from "@/lib/db";
 
-import { db } from "@/../../lib/db";
 import CourseSidebarItem from "./course-sidebar-item";
+import CourseProgress from "@/components/course-progress";
 
 interface CourseSidebarProps {
   course: Course & {
@@ -35,6 +36,11 @@ const CourseSidebar = async ({ course, progressCount }: CourseSidebarProps) => {
     <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
       <div className="p-8 flex flex-col border-b">
         <h1 className="font-semibold">{course.title}</h1>
+        {purchase && (
+          <div className="mt-10">
+            <CourseProgress variant="success" value={progressCount} />
+          </div>
+        )}
       </div>
       <div className="flex flex-col w-full">
         {course.chapters.map((chapter) => (
