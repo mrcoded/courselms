0405ffb9a1c132562778b1908347772ea1f02e76
CodedLibra <adeola.ogundijo@yahoo.com>
2026-01-13 23:56:36 +0100

    feat: created a PATCH route for courseId/unpublish aapi

---
+++ src/app/api/courses/[courseId]/unpublish/route.ts
@@ -0,0 +1,68 @@
+import { NextResponse } from "next/server";
+
+import { db } from "@/config/db";
+import { getServerSession } from "@/lib/get-server-session";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: Promise<{ courseId: string }> }
+) {
+  try {
+    //get user
+    const session = await getServerSession();
+    const user = session?.user;
+    const userId = user?.id;
+
+    //if user is not logged in
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    // Get courseId from params
+    const { courseId } = await params;
+
+    // Verify course ownership
+    const courseOwner = await db.course.findUnique({
+      where: {
+        id: courseId,
+        userId: userId,
+      },
+    });
+
+    // If user is not the owner of the course
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    // Verify course existence
+    const course = await db.course.findUnique({
+      where: {
+        id: courseId,
+        userId: userId,
+      },
+    });
+
+    // If course not found
+    if (!course) {
+      return new NextResponse("Not found", { status: 401 });
+    }
+
+    // Unpublish the course
+    const unPublishedCourse = await db.course.update({
+      where: {
+        id: courseId,
+        userId: userId,
+      },
+      data: {
+        isPublished: false,
+      },
+    });
+
+    return NextResponse.json(unPublishedCourse);
+  } catch (error) {
+    console.log("[COURSEID_UNPUBLISH]", error);
+    return new NextResponse("Internal Server Error", {
+      status: 500,
+    });
+  }
+}
