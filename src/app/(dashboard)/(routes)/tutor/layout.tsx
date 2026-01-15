c4d919cad15e371c3b737019f2e41e2b355f4665
CodedLibra <adeola.ogundijo@yahoo.com>
2026-01-15 22:44:54 +0100

    feat: created a layout for Tutor routes

---
+++ src/app/(dashboard)/(routes)/tutor/layout.tsx
@@ -0,0 +1,27 @@
+import { redirect } from "next/navigation";
+
+import { isTutor } from "@/lib/get-tutor";
+import { getServerSession } from "@/lib/get-server-session";
+
+export default async function TutorLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await getServerSession();
+  const user = session?.user;
+  const userId = user?.id;
+
+  // Redirect if not logged in
+  if (!session) redirect("/auth/login");
+
+  // If a tutor tries to enter the student dashboard
+  if (session.user.role === "student") {
+    redirect("/");
+  }
+
+  // Redirect if not logged in
+  if (!isTutor(userId)) return redirect("/");
+
+  return <>{children}</>;
+}
