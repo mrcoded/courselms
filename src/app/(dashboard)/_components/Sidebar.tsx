9e125ac7432e91e9b508fea78d92e61659e51f52
CodedLibra <adeola.ogundijo@yahoo.com>
2026-01-15 16:27:42 +0100

    feat: added logout button and function

--- src/app/(dashboard)/_components/Sidebar.tsx
+++ src/app/(dashboard)/_components/Sidebar.tsx
@@ -1,16 +1,34 @@
+"use client";
+
 import React from "react";
 import Logo from "./logo";
+import { LogOut } from "lucide-react";
+
 import SidebarRoutes from "./sidebar-routes";
+import { Button } from "@/components/ui/button";
+
+import { LogoutService } from "@/services/auth.service";
 
 const Sidebar = () => {
+  //handle logout
+  const { handleLogout } = LogoutService();
+
   return (
     <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
       <div className="p-6">
         <Logo />
       </div>
-      <div className="flex flex-col w-full">
+      <div className="flex flex-1 flex-col w-full">
         <SidebarRoutes />
       </div>
+      <Button
+        size="sm"
+        variant="ghost"
+        onClick={handleLogout}
+        className="flex justify-start items-center text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20"
+      >
+        <LogOut className="size-3 sm:size-4 mr-2" /> Logout
+      </Button>
     </div>
   );
 };
