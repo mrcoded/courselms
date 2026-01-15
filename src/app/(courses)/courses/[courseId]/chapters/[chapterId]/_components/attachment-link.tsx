c70784a799aaaec5da506afc21c0d172cd9fce2d
CodedLibra <adeola.ogundijo@yahoo.com>
2026-01-15 12:54:32 +0100

    feat: created an attachment-link component

---
+++ src/app/(courses)/courses/[courseId]/chapters/[chapterId]/_components/attachment-link.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import type { Attachment } from "@prisma/client";
+
+const AttachmentLink = ({ attachment }: { attachment: Attachment }) => {
+  return (
+    <a
+      href={attachment.url}
+      target="_blank"
+      key={attachment.id}
+      className="flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:underline"
+    >
+      <p className="line-clamp-1">{attachment.name}</p>
+    </a>
+  );
+};
+
+export default AttachmentLink;
