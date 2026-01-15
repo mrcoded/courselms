479245a7529401f3eda19c262ab6a35149c8b441
CodedLibra <adeola.ogundijo@yahoo.com>
2026-01-15 22:50:12 +0100

    feat: created a course-enroll-button component

---
+++ src/app/(courses)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React, { useState } from "react";
+import axios from "axios";
+
+import { Button } from "@/components/ui/button";
+import { toast } from "@/components/ui/use-toast";
+
+import { formatPrice } from "@/lib/formatPrice";
+import { CourseEnrollButtonProps } from "@/types/course.types";
+
+const CourseEnrollButton = ({ price, courseId }: CourseEnrollButtonProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      const response = await axios.post(`/api/courses/${courseId}/checkout`);
+
+      window.location.assign(response.data.url);
+    } catch {
+      toast({ title: "Error", description: "Something went wrong!" });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Button
+      onClick={onClick}
+      disabled={isLoading}
+      size="sm"
+      className="w-full md:w-auto text-xs sm:text-sm"
+    >
+      Enroll for {formatPrice(price)}
+    </Button>
+  );
+};
+
+export default CourseEnrollButton;
