47e6b28403013572799576f02b224186ff3994f1
CodedLibra <adeola.ogundijo@yahoo.com>
2026-01-15 22:48:08 +0100

    feat: created a chapter-dnd component

---
+++ src/app/(dashboard)/(routes)/tutor/courses/[courseId]/_components/chapter-dnd.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+import { Grip, Pencil } from "lucide-react";
+
+import {
+  Droppable,
+  Draggable,
+  DragDropContext,
+  DropResult,
+} from "@hello-pangea/dnd";
+
+import { Badge } from "@/components/ui/badge";
+
+import { useChapterStore } from "@/store/useChapterStore";
+
+const ChapterDND = ({
+  onDragEnd,
+  onEdit,
+}: {
+  onDragEnd: (result: DropResult) => void;
+  onEdit: (chapterId: string) => void;
+}) => {
+  //get chapters
+  const { chapters } = useChapterStore((state) => state);
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="chapters">
+        {(provided) => (
+          <div {...provided.droppableProps} ref={provided.innerRef}>
+            {chapters.map((chapter, index) => (
+              <Draggable
+                key={chapter.id}
+                draggableId={chapter.id}
+                index={index}
+              >
+                {(provided) => (
+                  <div
+                    className={cn(
+                      "flex items-center gap-x-2 bg-slate-200 border-slate-200 border text-slate-700 rounded-md mb-4 text-sm",
+                      chapter.isPublished &&
+                        "bg-sky-100 border-sky-200 text-sky-700"
+                    )}
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                  >
+                    <div
+                      className={cn(
+                        "px-2 py-3 border-r border-r-slate-200 hover:bg-slate-300 rounded-l-md transition",
+                        chapter.isPublished &&
+                          "border-r-sky-200 hover:bg-sky-200"
+                      )}
+                      {...provided.dragHandleProps}
+                    >
+                      <Grip className="h-5 w-5" />
+                    </div>
+                    {chapter.title}
+                    <div className="ml-auto pr-2 flex items-center gap-x-2">
+                      {chapter.isFree && <Badge>Free</Badge>}
+                      <Badge
+                        className={cn(
+                          "bg-slate-500",
+                          chapter.isPublished && "bg-sky-700"
+                        )}
+                      >
+                        {chapter.isPublished ? "Published" : "Draft"}
+                      </Badge>
+                      <Pencil
+                        onClick={() => onEdit(chapter.id)}
+                        className="w-4 h-4 cursor-pointer hover:opacity-75 transition"
+                      />
+                    </div>
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+};
+
+export default ChapterDND;
