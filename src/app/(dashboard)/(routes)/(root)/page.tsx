585a5f6a7ecbeecb2534e55b5950859b9cf20a48
CodedLibra <adeola.ogundijo@yahoo.com>
2026-01-16 15:03:52 +0100

    chore: homepage now redirects users based on role

--- src/app/(dashboard)/(routes)/(root)/page.tsx
+++ src/app/(dashboard)/(routes)/(root)/page.tsx
@@ -1,45 +1,21 @@
-import React from "react";
-import { Clock } from "lucide-react";
 import { redirect } from "next/navigation";
 
 import { getServerSession } from "@/lib/get-server-session";
-import { getDashboardCourses } from "@/lib/actions/get-dashboard-courses";
 
-import InfoCard from "./_components/dashboard-info-card";
-import CoursesList from "@/components/courses-list";
-
-const Dashboard = async () => {
+const HomePage = async () => {
   const session = await getServerSession();
   const userId = session?.user?.id;
+
+  const role = session?.user?.role;
 
   // Redirect if not logged in
   if (!userId) return redirect("/");
 
-  // Get dashboard datas
-  const { completedCourses, coursesInProgress } = await getDashboardCourses(
-    userId
-  );
+  // If user role is a tutor redirect
+  if (role === "tutor") return redirect("/tutor/courses");
 
-  return (
-    <div className="p-6 space-y-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        <InfoCard
-          icon={Clock}
-          label="In Progress"
-          numberOfItems={coursesInProgress.length}
-        />
-
-        <InfoCard
-          icon={Clock}
-          label="Completed"
-          numberOfItems={completedCourses.length}
-          variant="success"
-        />
-      </div>
-
-      <CoursesList items={[...coursesInProgress, ...completedCourses]} />
-    </div>
-  );
+  // If user role is student redirect
+  if (role === "student") return redirect("/dashboard");
 };
 
-export default Dashboard;
+export default HomePage;
